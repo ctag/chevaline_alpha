f3bccf033ea22ee078fec66fa8f0a916e29d2d01
Christopher Bero <bigbero@gmail.com>
2014-12-23 22:43:33 -0600

    maybe chrome ext is working, updated firefox version to 0.25

--- chrome/src/bg/background.js
+++ chrome/src/bg/background.js
@@ -1,11 +1,12 @@
 
 var keepaliveCount = 0;
-var URL1 = "https://sierra.uah.edu:9021/PROD/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu";
-var URL2 = "https://uah.instructure.com";
-var xhr = new XMLHttpRequest();
+var URL2 = "https://sierra.uah.edu:9021/PROD/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu";
+var URL1 = "https://uah.instructure.com";
+var xhr1 = new XMLHttpRequest();
+var xhr2 = new XMLHttpRequest();
 
 console.log("extension startup.");
-chrome.alarms.create("newAlarm" , {periodInMinutes : 0.2} );
+chrome.alarms.create("newAlarm" , {periodInMinutes : 0.5} );
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
@@ -14,34 +15,63 @@ chrome.extension.onMessage.addListener(
 	sendResponse();
 });
 
+/*
 chrome.runtime.onStartup.addListener(
 	function () {
-		//console.log("extension startup.");
-		//chrome.alarms.create("newAlarm" , {periodInMinutes : 0.2} );
+		console.log("extension startup via listener.");
+		chrome.alarms.create("newAlarm" , {periodInMinutes : 1.0} );
 	}
 );
+*/
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
 	
-	xhr.open("GET", URL1, true);
+	console.log("sending URL1 request");
 	
-	xhr.onreadystatechange = function() {
-		console.dir(xhr);
-		console.log("ready state: " + xhr.readyState);
+	xhr1.open("GET", URL1, false);
+	
+	xhr1.addEventListener("load", function () {
+		console.log("URL1 complete.");
+		console.log("URL1, response: " + xhr1.responseURL);
+		console.log("URL1, status: " + xhr1.status);
+	}, false);
+	
+	/*
+	xhr1.onreadystatechange = function() {
+		//console.dir(xhr);
+		console.log("URL1, readyState: " + xhr1.readyState);
+		console.log("URL1, response: " + xhr1.responseURL);
+		console.log("URL1, status: " + xhr1.status);
 	}
-	xhr.send();
+	*/
+	
+	xhr1.send();
 });
+
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
 	
-	xhr.open("GET", URL2, true);
+	console.log("sending URL2 request");
 	
-	xhr.onreadystatechange = function() {
-		console.dir(xhr);
-		console.log("ready state: " + xhr.readyState);
+	xhr2.open("GET", URL2, false);
+	
+	xhr2.addEventListener("load", function () {
+		console.log("URL2 complete.");
+		console.log("URL2, response: " + xhr2.responseURL);
+		console.log("URL2, status: " + xhr2.status);
+	}, false);
+	
+	/*
+	xhr2.onreadystatechange = function() {
+		//console.dir(xhr);
+		console.log("URL2, readyState: " + xhr2.readyState);
+		console.log("URL2, response: " + xhr2.responseURL);
+		console.log("URL2, status: " + xhr2.status);
 	}
-	xhr.send();
+	*/
+	xhr2.send();
 });
+
 
 
 
