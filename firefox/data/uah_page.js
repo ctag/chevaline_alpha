8e05aa6a38cd7fcd929e8a8d38019bcfbeec293b
Christopher Bero <csb0019@uah.edu>
2015-01-12 00:00:34 -0600

    broke the search function

--- firefox/data/uah_page.js
+++ firefox/data/uah_page.js
@@ -3,7 +3,6 @@
 console.log("page matches, running uah_page.js");
 
 //var data = $.parseJSON(courses);
-
 //console.log("course: " + courses.length);
 
 var colleges_num = new Array(courses.length);
@@ -11,23 +10,6 @@ var colleges_num = new Array(courses.length);
 for (var c = 0; c < colleges_num.length; c++)
 {
 	colleges_num[c] = courses[c].length;
-}
-
-//console.log("number of colleges in database: " + colleges_num.length);
-
-//console.log(colleges_num);
-
-var matched_elems = $('*:contains(data.ACC)');
-
-for (var i = 0; i < courses.length; i++)
-{
-	
-	for (var c = 1; c < courses[i].length; c++)
-	{
-		//
-	}
-	
-	//console.log(i + ": " + courses[i][0] + " - " + courses[i].length);
 }
 
 var page_text = $('body').text();
@@ -38,6 +20,10 @@ var myregex = /\b\w{2,3} \d{3}\b/gm;
 var str = "BLAH";
 //var mydata = page_text.replace(myregex, "BLAH");
 
+var college_regex = /\w{2,3}/;
+
+var page = $('body');
+var temp_html = page.html();
 
 do {
 var mydata = myregex.exec(page_text);
@@ -47,22 +33,25 @@ if (mydata == null)
 	break;
 }
 
-console.log("searching for: " + mydata);
-
-var college_regex = /\w{2,3}/;
 var college_regex_result = college_regex.exec(mydata);
+
+console.log("searching for: " + mydata);
 
 for (var c = 0; c < courses.length; c++)
 {
+	console.log("College: " + courses[c][0]);
 	if (courses[c][0] == college_regex_result)
 	{
+		console.log("\tCourses in collage: " + courses[c].length);
 		for (var i = 1; i < courses[c].length; i++)
 		{
+			console.log("checking: " + (courses[c][0] + " " + courses[c][i].course_num));
 			if ((courses[c][0] + " " + courses[c][i].course_num) == mydata)
 			{
 				console.log("Found a match: " + (courses[c][0] + " " + courses[c][i].course_num + " " + courses[c][i].course_sec));
-				var page = $('body');
-				page.html(page.html().replace(mydata, '<strong><div style="background: #AAFFAA; display: inline;">' + mydata + "</div></strong>"));
+				
+				temp_html = temp_html.replace(mydata, '<strong><div style="background: #AAFFAA; display: inline;">' + mydata + "</div></strong>");
+				break;
 			}
 		}
 	}	
@@ -71,6 +60,8 @@ for (var c = 0; c < courses.length; c++)
 //console.log(mydata);
 //console.log(mydata.length);
 }while (mydata != null);
+
+page.html(temp_html);
 
 console.log("Done searching");
 
