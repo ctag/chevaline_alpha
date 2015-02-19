0f07c0c87a707912317f55fc216fe72874e29bcd
Christopher Bero <bigbero@gmail.com>
2015-02-19 12:26:46 -0600

    plugin runs, need to test sso.js

--- firefox/lib/sso.js
+++ firefox/lib/sso.js
@@ -1,60 +1,82 @@
 /* SSO.JS - Single Sign On Script
- */
+*/
 
- var passwords = require('sdk/passwords');
- var simplePrefs = require('sdk/simple-prefs');
+var passwords = require('sdk/passwords');
+var simplePrefs = require('sdk/simple-prefs');
 
- var debug = simplePrefs.prefs['debug'];
+var ssoUser = '';
 
- function sso_setCredentials(_password, _user, _callback)
- {
-   if (debug) console.log("sso_setCredentials: ", _user, _password);
-     sso_clearCredentials(function (_cred) {
-       passwords.store({
-       realm: "chevaline_sso",
-       username: _user,
-       password: _password,
-       onError: function (_err) {
-         sso_handleError('sso_setCredentials', _err);
-       }
-     });
-   });
- }
+var debug = simplePrefs.prefs['debug'];
 
- function sso_clearCredentials(_callback)
- {
-   passwords.search({
-     realm: "chevaline_sso",
-     onComplete: function (_cred) {
-       _cred.forEach(passwords.remove);
-       if (debug) console.log("sso credentials: ", _cred);
-       _callback(_cred);
-     },
-     onError: function (_err) {
-       sso_handleError('sso_clearCredentials', _err);
-     }
-   });
- }
+function sso_setUsername(_user, _callback)
+{
+  if (debug) console.log("sso_setUsername: ", _user);
+  ssoUser = _user;
+}
 
- function sso_getCredentials()
- {
-   passwords.search({
-     realm: "chevaline_sso",
-     username: ssoUser,
-     onComplete: function (_cred) {
-         if (debug) console.log("sso credentials: ", _cred);
-     },
-     onError: function (_err) {
-       sso_handleError('sso_getCredentials', _err);
-     }
-   });
- }
+function sso_setPassword(_password, _callback)
+{
+  if (debug) console.log("sso_setPassword: ", _password);
+  function _error (_err) {
+    sso_handleError('sso_setPassword', _err);
+  }
+
+  function _addCred () {
+    passwords.store({
+      realm: "chevaline_sso",
+      username: _user,
+      password: _password,
+      onError: _error
+    });
+  }
+
+  sso_clearCredentials(_addCred);
+}
+
+function sso_clearCredentials(_callback)
+{
+  function _error (_err) {
+    sso_handleError('sso_clearCredentials', _err);
+  }
+
+  function _onComplete (_cred) {
+    _cred.forEach(passwords.remove);
+    if (debug) console.log("sso credentials: ", _cred);
+    _callback(_cred);
+  }
+
+  passwords.search({
+    realm: "chevaline_sso",
+    onComplete: _onComplete,
+    onError: _error
+  });
+}
+
+function sso_getCredentials(_callback)
+{
+  function _error (_err) {
+    sso_handleError('sso_getCredentials', _err);
+  }
+
+  function _onComplete (_cred) {
+    if (debug) console.log("sso credentials: ", _cred);
+    _callback(_cred);
+  }
+
+  passwords.search({
+    realm: "chevaline_sso",
+    username: ssoUser,
+    onComplete: _onComplete,
+    onError: _error
+  });
+}
 
 function sso_handleError (_source, _err)
 {
-  if (debug) console.log('Chevaline_Alpha; SSO Error: ', _source, _err.message);
+  if (debug) console.log('Chevaline_Alpha; ', _source, _err.message);
 }
 
-exports.Set = sso_setCredentials;
-exports.Get = sso_setCredentials;
-exports.Clear = sso_setCredentials;
+exports.SetUsername = sso_setUsername;
+exports.SetPassword = sso_setPassword;
+exports.Get = sso_getCredentials;
+exports.Clear = sso_clearCredentials;
